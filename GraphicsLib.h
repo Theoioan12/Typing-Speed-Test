61525864eb40ffc016df78dd8af5e7c2a9906ca1
Sorin Barbu <sbarbu38@yahoo.com>
2022-05-20 16:31:37 +0300

    Added structs in GraphicsLib.c and basic init and free screen in TypingSpeedTest.c

--- GraphicsLib.h
+++ GraphicsLib.h
@@ -3,8 +3,23 @@
 
     #include <stdlib.h>
     #include <string.h>
+    #include <inttypes.h>
+    #include <time.h>
     #include <ncurses.h>
 
+    typedef struct typeText{
+        char ch; // current letter
+        int8_t status; // letter status
+        time_t timestamp; // timestamp of typing
+    }typeText;
     
+    typedef struct levelData{
+        typeText *text;
+        uint32_t len;
+        uint32_t nr_tries;
+        uint32_t right;
+        float accuracy;
+        float WPM; 
+    }levelData;
 
 #endif
--- TypingSpeedTest.c
+++ TypingSpeedTest.c
@@ -1,45 +1,51 @@
-#include <stdlib.h>
-#include <ncurses.h>
+#include "GraphicsLib.h"
 
-void print(){
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-    mvwprintw(stdscr, yMax / 4, xMax / 2 - 12, ".:|| Website Finder ||:.");
-    mvwprintw(stdscr, yMax / 4 + 3, xMax / 2 - 12, ".:|| Website Finder ||:.");
-}
+void NCURSES_init();
+void NCURSES_free();
+
+
+
+void run();
 
 int main(){
 
-    initscr();
-    curs_set(1);
-    noecho();
-    start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_RED, COLOR_BLACK);
-    attron(COLOR_PAIR(1));
-    box(stdscr, 0, 0);
-    print();
-    int x = getch();
-    attron(COLOR_PAIR(2));
-    for(int i = 0; i < 3; i++){
-        box(stdscr, 0, 0);
-        int x = getch();
-        printw("%d ", x);
-        if(x == 127)
-            delch();
-        attron(COLOR_PAIR(1));
-        x = getch();
-        if(x == 127)
-            delch();
-        x = getch();
-        if(x == 127)
-            delch();
-        attron(COLOR_PAIR(2));
-        x = getch();
-        if(x == 127)
-            delch();
-        x = getch();
-    }
-    endwin();
+    run();
+
     return 0;
+}
+
+void NCURSES_init(){
+    // Initialize screen
+    initscr();
+    // Don't show cursor
+    curs_set(0);
+    // Don't disply input characters
+    noecho();
+    // Initialize color
+    start_color();
+    // Right color pair
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    // Wrong color pair
+    init_pair(2, COLOR_WHITE, COLOR_RED);
+}
+void NCURSES_free(){
+    // Free win
+    endwin();
+}
+
+
+
+void run(){
+    NCURSES_init();
+    attron(COLOR_PAIR(2));
+    char x = getch();
+    printw("%c ", x);
+    attron(COLOR_PAIR(1));
+    x = getch();
+    printw("%c ", x);
+    x = getch();
+    printw("%c ", x);
+    x = getch();
+    printw("%c ", x);
+    NCURSES_free();
 }
